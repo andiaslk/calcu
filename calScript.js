d3c7f995dcb66124d88a98a82144bb150f33b36c
andiaslk <angel.dianne.angeles02@gmail.com>
2025-06-03 16:12:05 +0800

    Add files via upload

---
+++ calScript.js
@@ -0,0 +1,20 @@
+const display = document.getElementById("display");
+
+function toClear(){
+    display.value = ' ';
+}
+function toDisplay(input){
+    display.value += input;
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value.replace(/÷/g, '/'));
+    }
+    catch{
+        display.value = "Error";
+    }
+} 
+function del(){
+    display.value = display.value.toString().slice(0, -1);
+}
